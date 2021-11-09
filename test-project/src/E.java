4fb71c5c4c0a50d34b8044676b0f99b56df657b7
Lena <walkers19@web.de>
2021-11-09 11:56:46 +0100

    add new instnce of existing clone class

---
+++ test-project/src/E.java
@@ -0,0 +1,29 @@
+public class E {
+    private int i;
+    private float f;
+    private char c;
+    private String s;
+
+    public E(int i, float f, char c, String s){
+        this.i = i;
+        this.f = f;
+        this.c = c;
+        this.s = s;
+    }
+
+    public char getC() {
+        return c;
+    }
+
+    public float getF() {
+        return f;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public String getS() {
+        return s;
+    }
+}
